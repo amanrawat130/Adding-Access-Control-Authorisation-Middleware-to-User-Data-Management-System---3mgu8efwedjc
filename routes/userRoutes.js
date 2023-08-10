457ba35ebc8c19803aede7e0f492e8f842cbc392
Aman Rawat <56511200+amanrawat130@users.noreply.github.com>
2023-08-10 14:47:32 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 const {
     getAllUsers,
@@ -6,25 +6,17 @@ const {
     createUser,
     updateUser,
     deleteUser,
-} = require("../controllers/userControllers");
+} = require('../controllers/userControllers');
 
-const grantAccessTo = require("../middlewares/grantAccessTo");
+const grantAccessTo = require('../middlewares/grantAccessTo');
 
 const router = express.Router();
 
-/*
-router.get("/"): Should be accessible to roles user, admin, and superadmin. This route is used to retrieve all users.
-router.get("/:id"): Should be accessible to roles user, admin, and superadmin. This route is used to retrieve a user by their ID.
-router.post("/"): Should be accessible to roles guest, user, admin, and superadmin. This route is used to create a new user.
-router.patch("/:id"): Should be accessible to roles admin and superadmin. This route is used to update an existing user.
-router.delete("/:id"): Should be accessible to role superadmin only. This route is used to delete an existing user.
-*/
-
-//Add middlewares here
-router.get("/", getAllUsers);
-router.get("/:id", getUserByID);
-router.post("/", createUser);
-router.patch("/:id", updateUser);
-router.delete("/:id", deleteUser);
+// Use grantAccessTo middleware for access control
+router.get('/', grantAccessTo(['user', 'admin', 'superadmin']), getAllUsers);
+router.get('/:id', grantAccessTo(['user', 'admin', 'superadmin']), getUserByID);
+router.post('/', grantAccessTo(['guest', 'user', 'admin', 'superadmin']), createUser);
+router.patch('/:id', grantAccessTo(['admin', 'superadmin']), updateUser);
+router.delete('/:id', grantAccessTo(['superadmin']), deleteUser);
 
 module.exports = router;
