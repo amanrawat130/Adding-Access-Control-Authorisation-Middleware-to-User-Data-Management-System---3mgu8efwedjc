dca5ca474aacce40bdc1aafdcc49f58549a0bae7
Aman Rawat <56511200+amanrawat130@users.noreply.github.com>
2023-08-10 14:46:18 +0530

    Update grantAccessTo.js

--- middlewares/grantAccessTo.js
+++ middlewares/grantAccessTo.js
@@ -13,13 +13,33 @@ Possible Cases:
 
 function grantAccessTo(roles) {
   return function (req, res, next) {
-    //Write your code here;
-    try{
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        return res.status(401).json({
+          message: 'Authentication failed: Missing token.',
+          status: 'Error',
+        });
+      }
+      
+      const decodedToken = jwt.verify(token, JWT_SECRET);
+      const userRole = decodedToken.role;
+      
+      if (!roles.includes(userRole)) {
+        return res.status(403).json({
+          message: 'Access Denied',
+          status: 'Error',
+        });
+      }
+
+      next();
     } catch (err) {
-      return res.status(401).json({ message: 'Authentication failed: Invalid token.', status: "Error" });
+      return res.status(401).json({
+        message: 'Authentication failed: Invalid token.',
+        status: 'Error',
+      });
     }
-  }
+  };
 }
 
 module.exports = grantAccessTo;
-
